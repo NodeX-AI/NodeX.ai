aeb1d929712b39d97735be6f81e1dba8954c75b2
wnderbin <ghostface.host@gmail.com>
2026-01-08 15:53:35 +0900

    v3.1.1 Support -> share added

---
+++ utils/share_text.py
@@ -0,0 +1,16 @@
+def get_share_text(lang: str = 'ru') -> str:
+    if lang == 'ru':
+        return (
+            "N​o​d​e​X.​a​i - Телеграм-бот с открытым исходным кодом, "
+            "предоставляющий доступ к множеству современных нейросетей. "
+            "Распознавание изображений и обработка текста в один клик.\n\n"
+            "@NodeX_aibot"
+        )
+    else:
+        return (
+            "N​o​d​e​X.​a​i - An open-source Telegram bot that "
+            "provides access to a variety of modern neural networks. "
+            "Image recognition and text processing in one click.\n\n"
+            "@NodeX_aibot"
+        )
+    
